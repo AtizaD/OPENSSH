c37e78f972777b832e576326d77cc0ab8a5ff2aa
DIANA ATIZA <142483387+AtizaD@users.noreply.github.com>
2023-09-17 13:55:38 +0100

    Update OPENSSH.sh

--- OPENSSH.sh
+++ OPENSSH.sh
@@ -3,10 +3,12 @@
 # Define colors for prompt
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+BLUE='\033[0;34m'  # Added blue color
 NC='\033[0m' # No Color
 
 # Prompt for a new port
-read -p "Enter the new SSH port: " new_port
+echo -e "${BLUE}Enter the new SSH port:${NC} "
+read -p "" new_port
 
 # Construct the sed command to replace the DEFAULT_HOST port in wsproxy.py
 sed_command_wsproxy="s/DEFAULT_HOST = \"127.0.0.1:[0-9]*\"/DEFAULT_HOST = \"127.0.0.1:$new_port\"/"
@@ -22,4 +24,3 @@ sudo sed -i "$sed_command_proxy" /etc/VPSManager/proxy.py
 
 # Display a colorful message
 echo -e "${GREEN}OPENSSH PORT updated to: $new_port${NC}"
-
