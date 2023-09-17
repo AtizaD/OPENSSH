8c2d30ac6ac3aafef9fa68ebda4483c9109a86a3
DIANA ATIZA <142483387+AtizaD@users.noreply.github.com>
2023-09-17 13:59:22 +0100

    Update OPENSSH.sh

--- OPENSSH.sh
+++ OPENSSH.sh
@@ -7,8 +7,7 @@ BLUE='\033[0;34m'  # Added blue color
 NC='\033[0m' # No Color
 
 # Prompt for a new port
-echo -e "${BLUE}Enter the new SSH port:${NC} "
-read -p "" new_port
+echo -e "${BLUE}Enter the new OPENSSH port:${NC} " read -p "" new_port
 
 # Construct the sed command to replace the DEFAULT_HOST port in wsproxy.py
 sed_command_wsproxy="s/DEFAULT_HOST = \"127.0.0.1:[0-9]*\"/DEFAULT_HOST = \"127.0.0.1:$new_port\"/"
